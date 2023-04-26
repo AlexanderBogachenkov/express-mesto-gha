a9fd59d704a2a1c367f2f426c1706e8caa27d973
Alexander Bogachenkov <sashako4etov@yandex.ru>
2023-04-26 20:32:12 +0300

    sprint 13 commit 0

---
+++ routes/cards.js
@@ -0,0 +1,10 @@
+// const express = require("express");
+// const { cards } = require("../data");
+
+// const cardsRouter = express.Router();
+
+// cardsRouter.get("/cards", (req, res) => {
+//   res.send({ data: cards });
+// });
+
+// module.exports = cardsRouter;
