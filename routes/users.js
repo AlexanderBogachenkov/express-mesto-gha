3069690cc3aa0ea2d3a61e9b3ff1b622f157cce4
Alexander Bogachenkov <sashako4etov@yandex.ru>
2023-05-06 03:26:04 +0300

    sprint 14 commit 1

--- routes/users.js
+++ routes/users.js
@@ -6,12 +6,17 @@ const {
   createUser,
   updateProfile,
   updateAvatar,
+  login,
+  getMe,
 } = require("../controllers/users");
 
 router.get("/", getUsers);
 router.get("/:userId", getUserById);
-router.post("/", createUser);
+
 router.patch("/me", updateProfile);
 router.patch("/me/avatar", updateAvatar);
+router.post("/signin", login);
+router.post("/signup", createUser);
+router.post("/users/me", getMe);
 
 module.exports = router;
