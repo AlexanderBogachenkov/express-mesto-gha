5e5948607778839fcea334db971d099ac1c7842f
Alexander Bogachenkov <sashako4etov@yandex.ru>
2023-05-10 21:58:01 +0300

    sprint 14 commit 2.7

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,8 @@ router.get("/", getUsers);
 router.get("/me", getMe);
 router.get("/:userId", celebrate({
   params: Joi.object().keys({
-    userId: Joi.string().hex().length(24).required(),
+    // userId: Joi.string().hex().length(24).required(),
+    userId: Joi.string().alphanum().length(24).hex(),
   }),
 }), getUserById);
 router.patch("/me", celebrate({
