f29efff31d41cb3880c3f58ed3a84f4fd45faf50
Alexander Bogachenkov <sashako4etov@yandex.ru>
2023-05-11 12:56:27 +0300

    sprint 14 commit 2.12 middlewares auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,14 +1,13 @@
 /* eslint-disable linebreak-style */
 const jwt = require("jsonwebtoken");
-
+const UnauthorizedError = require("../utils/UnauthorizedError");
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith("Bearer ")) {
-    return res
-      .status(401)
-      .send({ message: "Необходима авторизация" });
+    next(new UnauthorizedError("Необходима авторизация"));
+    return;
   }
 
   const token = authorization.replace("Bearer ", "");
@@ -16,9 +15,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, "some-secret-key");
   } catch (err) {
-    return res
-      .status(401)
-      .send({ message: "Необходима авторизация" });
+    throw new UnauthorizedError("Переданы неверные данные");
   }
 
   req.user = payload; // записываем пейлоуд в объект запроса
