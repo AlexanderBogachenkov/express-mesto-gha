fe3a7bffa1905b828bec504ebe9d08770a1d9663
Alexander Bogachenkov <sashako4etov@yandex.ru>
2023-05-11 13:06:30 +0300

    sprint 14 commit 2.14 routes users, util constant

--- routes/users.js
+++ routes/users.js
@@ -3,12 +3,13 @@ const { celebrate, Joi } = require("celebrate");
 const {
   getUsers, getUserById, getMe, updateProfile, updateAvatar,
 } = require("../controllers/users");
+const { regEx } = require("../utils/constants");
 
 router.get("/", getUsers);
 router.get("/me", getMe);
 router.get("/:userId", celebrate({
   params: Joi.object().keys({
-    userId: Joi.string().alphanum().length(24).hex(),
+    userId: Joi.string().hex().length(24).required(),
   }),
 }), getUserById);
 router.patch("/me", celebrate({
@@ -19,7 +20,8 @@ router.patch("/me", celebrate({
 }), updateProfile);
 router.patch("/me/avatar", celebrate({
   body: Joi.object().keys({
-    avatar: Joi.string().required().regex(/^https?:\/\/(www.)?([\da-z-]+\.)+\/?\S*/im),
+    // avatar: Joi.string().required().regex(/^https?:\/\/(www.)?([\da-z-]+\.)+\/?\S*/im),
+    avatar: Joi.string().required().regex(regEx),
   }),
 }), updateAvatar);
 
